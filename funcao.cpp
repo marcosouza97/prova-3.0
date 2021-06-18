b00f823226821353748e2f7173b26bb38be65598
MarcosAlves <82622925+marcosouza97@users.noreply.github.com>
2021-06-18 22:51:25 +0000

    Initial commit

---
+++ funcao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+struct tTriangulo{
+  float base=0.0;
+
+  void mostraArea(float x, float y){
+    cout << "Area da função eh: "<<(x*y)/2;
+
+  }
+
+  void mostraPerimetro()
+
+};
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "funcao.cpp"
+
+using namespace std;
+
+
+int main() {
+  
+  tTriangulo lado1,lado2,lado;
+
+
+  cout << "Digite o lado 1: " <<endl;
+  cin >> lado1.base;
+
+  cout << "Digite o lado 2: " << endl;
+  cin >> lado2.base;
+
+  cout << "Digite o lado 3: " << endl;
+  cin >> lado3.base
+
+  lado.mostraArea(lado1.base, lado2.base);
+
+}

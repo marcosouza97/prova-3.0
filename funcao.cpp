9367c9f7dd869e6efd7660bcba59ec85d1a37e53
Marcos Alves <82622925+marcosouza97@users.noreply.github.com>
2021-06-19 13:40:25 +0000

    construção 3

--- funcao.cpp
+++ funcao.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+// declaração da estrutura
 struct tTriangulo{
   float lado1=0.0;
   float lado2=0.0;
   float lado3=0.0;
+  float aux;
 
-  void mostraArea(float x, float y){
-    cout << "Area da função eh: "<<(x*y)/2<< endl;
+// função que vai receber as informações para o calculo da are
+  void mostraArea(float altura, float base){
+    cout << "Area da figura  eh: "<<(altura*base)/2<<" m²"<< endl;
+    cout << endl;
 
   }
-
-  void mostraPerimetro(float a, float b, float c){
-    cout << "O valor do perimetro eh: " << a+b+c << endl;
+// função que vai receber as informações para o perimetro
+  void mostraPerimetro(float co, float ca, float hp){
+    cout << "O valor do perimetro eh: " << co + ca + hp <<"m"<< endl;
+    cout <<endl;
   }
+
+
+// função que vai receber as informações para achar hipotenusa
+  void mostraHipotenusa(float catetoOposto, float catetoAdjacente){
+    float hipotenusa;
+
+    hipotenusa = sqrt(catetoOposto*catetoOposto + catetoAdjacente*catetoAdjacente);
+
+    cout << "valor da hipotenusa eh: " << hipotenusa<<endl;
+    cout << endl;
+  }
+
+//*******************************************************
+
+// Essa situação eh para caso seja informado apenas os valores dos catetos, ou seja sem a informação da hipotenusa
+
+
+void mostraCatetos(float cOposto,float cAdjacente){
+  
+  float hip = sqrt(cOposto*cOposto + cAdjacente*cAdjacente);
+
+  float perimetro = cAdjacente + cOposto + hip;
+
+  cout << "O valor da hipotenusa eh: " << hip << endl;
+  cout <<endl;
+ 
+  cout << "O valor do perimetro eh: " << perimetro <<"m" << endl;
+
+
+}
 
 };
--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "funcao.cpp"
 
+
 using namespace std;
 
 
 int main() {
   
+  // declaração das variaveis
   tTriangulo base, altura, hip,aux;
 
-
+  // Solicitando os valores ao usuario 
   cout << "Digite o lado 1: " <<endl;
   cin >> base.lado1;
 
@@ -18,7 +20,21 @@ int main() {
   cout << "Digite o lado 3: " << endl;
   cin >> hip.lado3;
 
+  //Chamando os caluclos da funcao.cpp
+  //calculo da area
   aux.mostraArea(base.lado1, altura.lado2);
+  
+  //calculo do perimetro
   aux.mostraPerimetro(base.lado1, altura.lado2, hip.lado3);
+
+  aux.mostraHipotenusa(base.lado1, altura.lado2);
+
+  // ***************************************************
+
+
+  //caso apenas os catetos fosse informados
+
+  aux.mostraCatetos(base.lado1, altura.lado2);
+  
 
 }
